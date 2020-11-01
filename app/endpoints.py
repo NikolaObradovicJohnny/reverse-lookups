53bf6de099242bffd4ad16a6047d51e1c46c561b
Nikola Obradović <obradovic.nix@gmail.com>
2020-11-02 00:34:57 +0100

    Added CORS and concurrent futures

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,11 +1,17 @@
 from app import app
-from app.helpers import get_ip_network, reverse_lookups, reverse_lookups_socket
+from app.helpers import get_ip_network, reverse_lookups, reverse_lookups_socket, reverse_lookups_socket_async
 from flask import request, jsonify
 import asyncio
 
 
 loop = asyncio.get_event_loop()
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.route("/reverse-lookups")
 def get_reverse_lookups():
@@ -36,6 +42,25 @@ def get_reverse_lookups_with_socket():
         return jsonify({"error": "CIDR is not in valid format."})
     
     resolved = loop.run_until_complete(reverse_lookups_socket(pattern, ip_network))
+
+    response = {
+        "ip_addresses": resolved,
+        "totalResolved": len(resolved),
+        "totalMatched": len(list(filter(lambda x: x['match'], resolved))),
+        "total": len(ip_network)
+    }
+    return jsonify(response)
+
+@app.route("/reverse-lookups/async")
+def get_reverse_lookups_with_socket_async():
+    cidr = request.args.get("cidr")
+    pattern = request.args.get("search") or ""
+    
+    ip_network = get_ip_network(cidr)
+    if ip_network is None:
+        return jsonify({"error": "CIDR is not in valid format."})
+    
+    resolved = reverse_lookups_socket_async(pattern, ip_network)
 
     response = {
         "ip_addresses": resolved,
--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,6 @@
 from netaddr import *
 import asyncio
+import concurrent.futures
 import re
 import socket
 
@@ -29,6 +30,33 @@ async def reverse_lookups_socket(pattern, ip_network):
     return resolved
 
 async def check_matching(pattern, hostname, ip_address):
+    match_pattern = re.match(pattern, hostname)
+    return {
+        'hostname': hostname,
+        'ip': ip_address,
+        'match': not not match_pattern
+    }
+
+def reverse_lookups_socket_async(pattern, ip_network):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        resolved = []
+        for ip_address in ip_network:
+            futures.append(executor.submit(check_ip_address, ip_address=str(ip_address), pattern=pattern))
+        for future in concurrent.futures.as_completed(futures):
+            future_result = future.result()
+            if future_result is not None:
+                resolved.append(future_result)
+    return resolved
+
+def check_ip_address(ip_address, pattern):
+    try:
+        hostname = socket.gethostbyaddr(ip_address)
+        return check_matching_async(pattern, hostname[0], ip_address)
+    except:
+        pass
+
+def check_matching_async(pattern, hostname, ip_address):
     match_pattern = re.match(pattern, hostname)
     return {
         'hostname': hostname,
